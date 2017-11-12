01f7cca96ca913f0670dcbb623b4092bc8875279
FigVamPR <figvampr141@gmail.com>
2017-11-12 20:56:51 +0200

    adding page technologies

---
+++ technologies.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>technoilogie</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-fluid">
+                <header>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2>TECHNO<br>LOGIES</h2>
+                        </div>
+                    </div>
+                </header>
+                <article>
+                    <div class="row">
+                        <div class="col-md-2 menu"><!--menu-->
+                            <div class="group">
+                                <a href="about.php">
+                                    <img src="images/menuAbout.png" alt="about" class="img-menu">
+                                    <div class="caption">
+                                        <h3>about</h3>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="group">
+                                <a href="services.php">
+                                    <img src="images/menuServices.png" alt="services" class="img-menu">
+                                    <div class="caption">
+                                        <h3>services</h3>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="group active">
+                                <a href="technologies.php">
+                                    <img src="images/menuTechnologie.png" alt="technologie" class="img-menu">
+                                    <div class="caption">
+                                        <h3>technologies</h3>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="group">
+                                <a href="portfolio.php">
+                                    <img src="images/menuPortfolio.png" alt="portfolio" class="img-menu">
+                                    <div class="caption">
+                                        <h3>portfolio</h3>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="group">
+                                <a href="contacts.php">
+                                    <img src="images/menuContacts.png" alt="contacts"  class="img-menu">
+                                    <div class="caption">
+                                        <h3>contacts</h3>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-10 main"><!--technologie content-->
+                            <div class="row row-technologie">
+                                <div class="col-md-3 group-article">
+                                    <a href="#" class="thumbnail">
+                                        <img src="images/technologies/php.png" alt="php" class="image-content">
+                                    </a>
+                                </div>
+                                <div class="col-md-3 group-article">
+                                    <a href="#" class="thumbnail">
+                                        <img src="images/technologies/js.png" alt="js" class="image-content">
+                                    </a>
+                                </div>
+                                <div class="col-md-3 group-article">
+                                    <a href="#" class="thumbnail">
+                                        <img src="images/technologies/bash.png" alt="bash" class="image-content">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="row row-technologie">
+                                <div class="col-md-3 group-article">
+                                    <a href="#" class="thumbnail">
+                                        <img src="images/technologies/ruby.png" alt="ruby" class="image-content">
+                                    </a>
+                                </div>
+                                <div class="col-md-3 group-article">
+                                    <a href="#" class="thumbnail">
+                                        <img src="images/technologies/html.png" alt="html" class="image-content">
+                                    </a>
+                                </div>
+                                <div class="col-md-3 group-article">
+                                    <a href="#" class="thumbnail">
+                                        <img src="images/technologies/css.png" alt="css" class="image-content">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="row row-technologie">
+                                <div class="col-md-3 group-article">
+                                    <a href="#" class="thumbnail">
+                                        <img src="images/technologies/jquery.png" alt="jquery" class="image-content">
+                                    </a>
+                                </div>
+                                <div class="col-md-3 group-article">
+                                    <a href="#" class="thumbnail">
+                                        <img src="images/technologies/ubuntu.png" alt="ubuntu" class="image-content">
+                                    </a>
+                                </div>
+                                <div class="col-md-3 group-article">
+                                    <a href="#" class="thumbnail">
+                                        <img src="images/technologies/git.png" alt="git" class="image-content">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            <footer>
+                <h2><span class="glyphicon glyphicon-copyright-mark"></span> Figvam Team 2017</h2>
+            </footer>
+        </div>
+    </body>
+</html>
