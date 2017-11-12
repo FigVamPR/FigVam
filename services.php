e1484320fc89c8bb07ab3343da7af87d445a2d14
FigVamPR <figvampr141@gmail.com>
2017-11-12 20:55:38 +0200

    adding page services

---
+++ services.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>technoilogie</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="container-fluid">
+    <header>
+        <div class="row">
+            <div class="col-md-12">
+                <h2>SERVICES</h2>
+            </div>
+        </div>
+    </header>
+    <article>
+        <div class="row">
+            <div class="col-md-2 menu"><!--menu-->
+                <div class="group">
+                    <a href="about.php">
+                        <img src="images/menuAbout.png" alt="about" class="img-menu">
+                        <div class="caption">
+                            <h3>about</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="group">
+                    <a href="services.php">
+                        <img src="images/menuServices.png" alt="services" class="img-menu">
+                        <div class="caption">
+                            <h3>services</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="group active">
+                    <a href="technologies.php">
+                        <img src="images/menuTechnologie.png" alt="technologie" class="img-menu">
+                        <div class="caption">
+                            <h3>technologies</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="group">
+                    <a href="portfolio.php">
+                        <img src="images/menuPortfolio.png" alt="portfolio" class="img-menu">
+                        <div class="caption">
+                            <h3>portfolio</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="group">
+                    <a href="contacts.php">
+                        <img src="images/menuContacts.png" alt="contacts"  class="img-menu">
+                        <div class="caption">
+                            <h3>contacts</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-10 main"><!--technologie content-->
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="thumbnail">
+                            <img src="images/services/designer.png" alt="designer">
+                            <div class="caption">
+                                <h3>Євстаф'єва Євгенія</h3>
+                                <p>Наш улюблений програміст і дизайнер.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="thumbnail">
+                            <img src="images/services/fullstack.png" alt="fullstack">
+                            <div class="caption">
+                                <h3>Єльцин Яніс</h3>
+                                <p>Наш Frontend та початковий Backend розробник.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="thumbnail">
+                            <img src="images/services/fullstackstart.png" alt="fullstack">
+                            <div class="caption">
+                                <h3>Атаман Влад</h3>
+                                <p>Початківець у всіх напрямках.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="thumbnail">
+                            <img src="images/services/marketolog.png" alt="marketolog">
+                            <div class="caption">
+                                <h3>Боднар Олексій</h3>
+                                <p>Наш юний маркетолог.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+    <footer>
+        <h2><span class="glyphicon glyphicon-copyright-mark"></span> Figvam Team 2017</h2>
+    </footer>
+</div>
+</body>
+</html>
